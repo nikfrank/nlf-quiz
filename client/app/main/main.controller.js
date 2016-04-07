4ed613bffd150fc8aa6aa54cc9572c2fa9f95e92
Noa Klerman <Noa.Klerman@clearforest.com>
2016-04-07 22:26:06 +0300

    css for quiz

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -10,38 +10,119 @@ class MainController {
 	var b={};
 	this.awesomeThings=["noa1",1,b.k=0,"nik"]
 
+	
 	this.categories = [
-	  {
-		title:'faggots',
-		questions:[
-			{text:'what is this shit?', answer:'some fucking fuck', show:'value',value:100},
-			{text:'who is this goddamned faggot?', answer:'some fucking fuck', show:'value',value:200},
-			{text:'who is this cunt?', answer:'some fucking fuck', show:'value',value:300},
-			{text:'what is this fucking shit?', answer:'some fucking fuck', show:'value',value:400},
-			{text:'what is this dick?', answer:'some fucking fuck', show:'value',value:500}
-		]
-	  },
-	  {
-		title:'monkeyfuckers',
-		questions:[
-			{text:'what is this shit?', answer:'some fucking fuck', show:'value',value:100},
-			{text:'who is this goddamned faggot?', answer:'some fucking fuck', show:'value',value:200},
-			{text:'who is this cunt?', answer:'some fucking fuck', show:'value',value:300},
-			{text:'what is this fucking shit?', answer:'some fucking fuck', show:'value',value:400},
-			{text:'what is this dick?', answer:'some fucking fuck', show:'value',value:500}
-		]
-	  },
-	  {
-		title:'princesses',
-		questions:[
-			{text:'what is this shit?', answer:'some fucking fuck', show:'value',value:100},
-			{text:'who is this goddamned faggot?', answer:'some fucking fuck', show:'value',value:200},
-			{text:'who is this cunt?', answer:'some fucking fuck', show:'value',value:300},
-			{text:'what is this fucking shit?', answer:'some fucking fuck', show:'value',value:400},
-			{text:'what is this dick?', answer:'some fucking fuck', show:'value',value:500}
-		]
-	  }
-	];
+  {
+    name: 'organ system function',
+    questions:[
+      {
+        value:100,
+		show:'value',
+        text:'mechanical breakdown of food',
+        answer:'what is digestive system'
+      },
+      {
+        value:200,
+		show:'value',
+        text:'survival of genes',
+        answer:'what is reproductive system'
+      },
+      {
+        value:300,
+		show:'value',
+        text:'protection',
+        answer:'what is integumentary system'
+      },
+      {
+        value:400,
+		show:'value',
+        text:'movement',
+        answer:'what is muscular system'
+      },
+      {
+        value:500,
+		show:'value',
+        text:'communication between body organs',
+        answer:'what is nervous system'
+      }
+    ]
+  },
+  {
+    name:'epithelial tissue types',
+    questions:[
+      {
+        value:100,
+		show:'value',
+        text:'scalelike cells',
+        answer:'what is squamous'
+      },
+      {
+        value:200,
+		show:'value',
+        text:'taller than wide',
+        answer:'what is columnar'
+      },
+      {
+        value:300,
+		show:'value',
+        text:'single layer cube shaped',
+        answer:'what is simple cuboidal'
+      },
+      {
+        value:400,
+		show:'value',
+        text:'many layers flat cells',
+        answer:'what is stratified squamous'
+      },
+      {
+        value:500,
+		show:'value',
+        text:'many layers varying shaped cells',
+        answer:'what is stratified transitional'
+      }
+    ]
+  },
+  {
+    name:'connective tissue types',
+    questions:[
+      {
+        value:100,
+		show:'value',
+        text:'contains RBC\'s',
+        answer:'what is blood'
+      },
+      {
+        value:200,
+		show:'value',
+        text:'white or brown',
+        answer:'what is adipose'
+      },
+      {
+        value:300,
+		show:'value',
+        text:'contains calcium mineral crystals',
+        answer:'what is bone'
+      },
+      {
+        value:400,
+		show:'value',
+        text:'contains chondrocytes',
+        answer:'what is cartilage'
+      },
+      {
+        value:500,
+		show:'value',
+        text:'blood-forming tissue',
+        answer:'what is hematopiotic'
+      }
+    ]
+  }
+];
+	
+	
+	
+	
+
 
 	this.goTo = function(question, newShowValue){
 		question.show = newShowValue
